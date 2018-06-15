72f3720d61f5e25ccafc36d07a132e3f575ad10c
szter <sthrpx.e@gmail.com>
2018-06-14 21:01:23 -0300

    add changes

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,2 +1,8 @@
-// let's go!
-alert('hey');
+import React from 'react';
+import { render } from 'react-dom';
+/* import StorePicker from './components/StorePicker'; */
+/* import App from './components/App'; */
+import Router from './components/Router';
+import './css/style.css';
+
+render(<Router />, document.getElementById('main'));
